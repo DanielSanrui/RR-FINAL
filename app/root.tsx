f7f4a74c21b44ad6faf637ca7e99643ccc4a6fe7
DanielSanrui <daniel.sanc.ruiz@gmail.com>
2025-03-11 19:04:47 +0100

    second commit

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -29,6 +30,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        />
         <Meta />
         <Links />
       </head>
